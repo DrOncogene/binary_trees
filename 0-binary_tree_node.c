5253ea9bf6ac00768bd8ecd43a5429e960d6c8cb
Nasir Abdulrasheed <mypythtesting@gmail.com>
2022-02-15 11:44:17 +0100

    function to create a new node

---
+++ 0-binary_tree_node.c
@@ -0,0 +1,22 @@
+#include "binary_trees.h"
+
+/**
+  * binary_tree_node - creates a new binary tree node
+  * @parent: parent of the new node
+  * @value: value to store in the new node
+  * Return: Pointer ot the new node
+  */
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
+{
+	binary_tree_t *new;
+
+	new = malloc(sizeof(binary_tree_t));
+	if (new == NULL)
+		return (NULL);
+	new->parent = parent;
+	new->n = value;
+	new->left = NULL;
+	new->right = NULL;
+
+	return (new);
+}
